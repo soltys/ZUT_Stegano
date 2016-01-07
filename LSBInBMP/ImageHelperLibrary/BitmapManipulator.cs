3919da4d592bc0c191d2bac24828c76f60903cbe
Paweł Sołtysiak <soltys[[AT]]uznam[[DOT]]org>
2016-01-07 10:21:48 +0100

    code init

---
+++ LSBInBMP/ImageHelperLibrary/BitmapManipulator.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageHelperLibrary
+{
+    public class Pixel
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Color Color { get; set; }
+        public byte[] ColorBytes
+        {
+            get { return BitConverter.GetBytes(Color.ToArgb()); }
+        }
+
+        public byte Red
+        {
+            get { return ColorBytes[0]; }
+        }
+
+        public byte Green
+        {
+            get { return ColorBytes[1]; }
+        }
+
+        public byte Blue
+        {
+            get { return ColorBytes[2]; }
+        }
+
+
+    }
+
+    public class BitmapPixelEnumerator : IEnumerator<Pixel>, IEnumerable<Pixel>
+    {
+        private Bitmap _bitmap;
+        private int _y;
+        private int _x;
+
+        public BitmapPixelEnumerator(Bitmap bitmap)
+        {
+            _bitmap = bitmap;
+            Reset();
+        }
+        public void Dispose()
+        {
+
+        }
+
+        public bool MoveNext()
+        {
+            var isDone = _x == _bitmap.Width && _y == _bitmap.Height;
+
+            _x++;
+            if (_bitmap.Width == _x)
+            {
+                _x = 0;
+                _y++;
+            }
+
+            return !isDone;
+        }
+
+        public void Reset()
+        {
+            _x = 0;
+            _y = 0;
+        }
+
+        public Pixel Current
+        {
+            get
+            {
+                var p = new Pixel();
+                p.X = _x;
+                p.Y = _y;
+                p.Color = _bitmap.GetPixel(_x, _y);
+                return p;
+            }
+
+        }
+
+        object IEnumerator.Current
+        {
+            get { return Current; }
+        }
+
+        public IEnumerator<Pixel> GetEnumerator()
+        {
+            return this;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+    public class BitmapManipulator
+    {
+        private Bitmap _img;
+        private Bitmap _copy;
+
+
+        public BitmapManipulator(byte[] data)
+        {
+            using (MemoryStream ms = new MemoryStream(data))
+            {
+                _img = new Bitmap(ms);
+                _copy = new Bitmap(ms);
+            }
+
+        }
+
+        public void Strike()
+        {
+            for (int i = 0; i < _img.Width; i++)
+            {
+                _img.SetPixel(i, i, Color.Black);
+            }
+
+        }
+
+        public byte[] Bytes
+        {
+            get
+            {
+                byte[] data;
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    _copy.Save(ms, ImageFormat.Bmp);
+                    data = ms.ToArray();
+
+                }
+
+                return data;
+
+            }
+        }
+
+        public void InsertMessage(string message)
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            var dataToEncode = Encoding.ASCII.GetBytes(message);
+            byte[] dataWithMeta = new byte[dataToEncode.Length + 4 + 2];
+            //four bytes for message length; 2 bytes for padding
+            var messageLengthInfo = BitConverter.GetBytes(message.Length);
+            Array.Copy(messageLengthInfo, dataWithMeta, 4);
+            Array.Copy(dataToEncode, 0, dataWithMeta, 6, dataToEncode.Length);
+
+            BitArray bits = new BitArray(dataWithMeta);
+            var e = bits.GetEnumerator();
+            var bmpPixelEnumerator = new BitmapPixelEnumerator(_img);
+            foreach (Pixel pixel in bmpPixelEnumerator)
+            {
+
+                byte oldBlue = pixel.Blue;
+                byte oldGreen = pixel.Green;
+                byte oldRed = pixel.Red;
+
+                byte newBlue;
+                if (InsertData(e, oldBlue, out newBlue)) return;
+                _copy.SetPixel(pixel.X, pixel.Y, Color.FromArgb(newBlue, oldGreen, oldRed));
+
+
+                byte newGreen;
+                if (InsertData(e, oldGreen, out newGreen)) return;
+                _copy.SetPixel(pixel.X, pixel.Y, Color.FromArgb(newBlue, newGreen, oldRed));
+
+
+                byte newRed;
+                if (InsertData(e, oldRed, out newRed)) return;
+                _copy.SetPixel(pixel.X, pixel.Y, Color.FromArgb(newBlue, newGreen, newRed));
+            }
+            watch.Stop();
+            Debug.WriteLine(watch.Elapsed);
+        }
+
+
+
+        private bool InsertData(IEnumerator e, byte oldBlue,
+            out byte newBlue)
+        {
+            bool end = e.MoveNext();
+            if (!end)
+            {
+                newBlue = oldBlue;
+                return true;
+            }
+            var x1 = (bool)e.Current;
+            e.MoveNext();
+            var x2 = (bool)e.Current;
+
+            var blueBits = new BitArray(new[] { oldBlue });
+
+            var a1 = blueBits[0];
+            var a2 = blueBits[1];
+            var a3 = blueBits[2];
+            var t1 = (x1 == a1 ^ a3);
+            var t2 = (x2 == a2 ^ a3);
+            if (t1 && t2)
+            {
+            }
+            if (!t1 && t2)
+            {
+                blueBits[0] = !blueBits[0];
+            }
+            if (t1 && !t2)
+            {
+                blueBits[1] = !blueBits[1];
+            }
+            if (!t1 && !t2)
+            {
+                blueBits[2] = !blueBits[2];
+            }
+
+            newBlue = ConvertColorBitsToByte(blueBits);
+            return false;
+        }
+
+        byte ConvertColorBitsToByte(BitArray bits)
+        {
+            if (bits.Count != 8)
+            {
+                throw new ArgumentException("bits");
+            }
+            byte[] bytes = new byte[1];
+            bits.CopyTo(bytes, 0);
+            return bytes[0];
+        }
+
+        int ConvertBitsToWord(BitArray bits)
+        {
+            if (bits.Count < 32 && bits.Count > 48)
+            {
+                throw new ArgumentException("bits");
+            }
+            byte[] bytes = new byte[6];
+            bits.CopyTo(bytes, 0);
+            return BitConverter.ToInt32(bytes, 0);
+        }
+
+
+        /// <summary>
+        /// Expected 471 length
+        /// </summary>
+        /// <returns></returns>
+        public string ReadMessage()
+        {
+            var bmpPixelEnumerator = new BitmapPixelEnumerator(_img);
+            var encodedLength = bmpPixelEnumerator.Take(8).ToArray();
+
+            int indexLengthBit = 0;
+            var ba = new BitArray(48);
+            foreach (var pixel in encodedLength)
+            {
+                indexLengthBit = ReadTwoBits(pixel.Blue, ba, indexLengthBit);
+                indexLengthBit = ReadTwoBits(pixel.Green, ba, indexLengthBit);
+                indexLengthBit = ReadTwoBits(pixel.Red, ba, indexLengthBit);
+            }
+
+            var messageLength = ConvertBitsToWord(ba);
+
+            int indexMessageBit = 0;
+            var messageBitArray = new BitArray(messageLength * 8);
+            bmpPixelEnumerator.Reset();
+            var encodedPixels = bmpPixelEnumerator.Skip(8).Take((int)Math.Ceiling(messageLength * 8d / 6d));
+            foreach (var pixel in encodedPixels)
+            {
+                indexMessageBit = ReadTwoBits(pixel.Blue, messageBitArray, indexMessageBit);
+                if (indexMessageBit >= messageBitArray.Count)
+                {
+                    break;
+                }
+                indexMessageBit = ReadTwoBits(pixel.Green, messageBitArray, indexMessageBit);
+                if (indexMessageBit >= messageBitArray.Count)
+                {
+                    break;
+                }
+                indexMessageBit = ReadTwoBits(pixel.Red, messageBitArray, indexMessageBit);
+                if (indexMessageBit >= messageBitArray.Count)
+                {
+                    break;
+                }
+            }
+            
+            byte[] messageBytes = new byte[messageLength];
+            messageBitArray.CopyTo(messageBytes,0);
+            var message = Encoding.ASCII.GetString(messageBytes);
+
+            return message;
+        }
+
+        private static int ReadTwoBits(byte redByte, BitArray ba, int indexMessageBit)
+        {
+            var redBits = new BitArray(new[] { redByte });
+
+            var a1 = redBits[0];
+            var a2 = redBits[1];
+            var a3 = redBits[2];
+
+            var x1 = a1 ^ a3;
+            ba.Set(indexMessageBit++, x1);
+            var x2 = a2 ^ a3;
+            ba.Set(indexMessageBit++, x2);
+            return indexMessageBit;
+        }
+    }
+}
---
+++ LSBInBMP/LSBInBMP/MainWindowViewModel.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Xml.Serialization;
+using ImageHelperLibrary;
+using LSBInBMP.View.ViewModel;
+using Microsoft.Win32;
+using Xceed.Wpf.Toolkit;
+using MessageBox = System.Windows.MessageBox;
+
+namespace LSBInBMP
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private ICommand _openFileCommand;
+        private BitmapImage _sourceImage;
+        private BitmapImage _cryptedImageSource = new BitmapImage();
+        private RelayCommand _insertMessage;
+        private RelayCommand _readMessage;
+        private RelayCommand _saveFile;
+
+        public MainWindowViewModel()
+        {
+            _openFileCommand = new RelayCommand(OpenFileAction);
+            _sourceImage = new BitmapImage(new Uri("/source.bmp", UriKind.Relative));
+            
+            _message =
+                @"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tristique commodo neque ac venenatis. Donec tincidunt orci at turpis vulputate euismod. Vestibulum tempor interdum massa, non tincidunt nisl egestas ac. Integer iaculis, leo vitae laoreet porttitor, metus urna maximus ipsum, a luctus ligula dolor eu lectus. Fusce a libero eleifend, volutpat mi ut, scelerisque quam. Nulla facilisi. Donec et egestas felis. Aenean sollicitudin lorem non mi laoreet rhoncus.";
+
+            _insertMessage = new RelayCommand(InsertMessageAction);
+            _readMessage = new RelayCommand(ReadMessageAction);
+            _saveFile = new RelayCommand(SaveFileAction);
+
+        }
+
+        private void SaveFileAction(object obj)
+        {
+            MemoryStream ms = new MemoryStream();
+            BitmapEncoder encode = new BmpBitmapEncoder();
+            encode.Frames.Add(BitmapFrame.Create(_cryptedImageSource));
+            encode.Save(ms);
+            var bmpData = ms.ToArray();
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "BMP Files (*.bmp)|*.bmp";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllBytes(saveFileDialog.FileName, bmpData);
+            }
+
+        }
+
+
+        private void ReadMessageAction(object obj)
+        {
+            MemoryStream ms = new MemoryStream();
+            BitmapEncoder encode = new BmpBitmapEncoder();
+
+            encode.Frames.Add(BitmapFrame.Create(_sourceImage));
+            encode.Save(ms);
+            var bmpData = ms.ToArray();
+            BitmapManipulator manipulator = new BitmapManipulator(bmpData);
+            var messageRead = manipulator.ReadMessage();
+
+            MessageBox.Show(messageRead, "Odczytana wiadomość");
+        }
+
+        private static byte[] ReadFully(Stream input)
+        {
+            byte[] buffer = new byte[16 * 1024];
+            using (MemoryStream ms = new MemoryStream())
+            {
+                int read;
+                while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    ms.Write(buffer, 0, read);
+                }
+                return ms.ToArray();
+            }
+        }
+        private void InsertMessageAction(object parameter)
+        {
+            string message = parameter as string;
+
+            MemoryStream ms = new MemoryStream();
+            BitmapEncoder encode = new BmpBitmapEncoder();
+            
+            encode.Frames.Add(BitmapFrame.Create(_sourceImage));            
+            encode.Save(ms);
+            var bmpData = ms.ToArray();
+            BitmapManipulator manipulator = new BitmapManipulator(bmpData);
+            manipulator.InsertMessage(message);
+
+            var imageSource = new BitmapImage();
+            MemoryStream imageMS = new MemoryStream(manipulator.Bytes);
+
+            imageSource.BeginInit();
+            imageSource.StreamSource = imageMS;
+            imageSource.EndInit();
+
+            CryptedImage = imageSource;
+            if (bmpData.Length != manipulator.Bytes.Length)
+            {
+                Debugger.Break();
+            }
+
+        }
+
+        public ICommand OpenFile
+        {
+            get { return _openFileCommand; }
+        }
+
+        public BitmapImage SourceImage
+        {
+            get { return _sourceImage; }
+            set
+            {
+                _sourceImage = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public BitmapImage CryptedImage
+        {
+            get { return _cryptedImageSource; }
+            set
+            {
+                _cryptedImageSource = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand InsertMessage
+        {
+            get { return _insertMessage; }
+        }
+
+        public ICommand ReadMessage
+        {
+            get { return _readMessage; }
+        }
+
+        public ICommand SaveFile
+        {
+            get { return _saveFile; }
+        }
+
+        private string _message;
+        public string Message
+        {
+            get { return _message; }
+            set { _message = value; OnPropertyChanged(); }
+
+        }
+
+
+        private string _password;
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; OnPropertyChanged(); }
+
+        }
+
+        private string _stegano;
+        public string Stegano
+        {
+            get { return _stegano; }
+            set { _stegano = value; OnPropertyChanged(); }
+
+        }
+
+        public void OpenFileAction(object sender)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+
+
+            // Set filter for file extension and default file extension 
+            dlg.DefaultExt = ".bmp";
+            dlg.Filter = "BMP Files (*.bmp)|*.bmp";
+
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            Nullable<bool> result = dlg.ShowDialog();
+
+
+            // Get the selected file name and display in a TextBox 
+            if (result == true)
+            {
+                // Open document 
+                string filename = dlg.FileName;
+                SourceImage = new BitmapImage(new Uri(filename));
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
